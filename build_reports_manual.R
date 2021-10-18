caa2488dbf0687e8f2b332823000768eb43a8293
Paolo Martini <paolo.cavei@gmail.com>
2021-10-18 08:49:23 +0200

    Fix report builder

--- build_reports_manual.R
+++ build_reports_manual.R
@@ -1,4 +1,4 @@
 library(PharmGenBS)
 
-create_reports(file_info = "samples_info.tsv", file_genos = "samples_genos.tsv")
-create_internal_reports(file_info = "samples_info.tsv", file_genos = "samples_genos.tsv")
+PharmGenBS::create_reports(file_info = "samples_info.tsv", file_genos = "samples_genos.tsv")
+PharmGenBS::create_internal_reports(file_info = "samples_info.tsv", file_genos = "samples_genos.tsv")
